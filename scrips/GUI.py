4e51d576e6c57756edd10628cac96e9431e3219e
MateuszMagd <240742@edu.p.lodz.pl>
2023-07-13 02:17:49 +0200

    Visual/Practical changes

--- scrips/GUI.py
+++ scrips/GUI.py
@@ -2,12 +2,20 @@ from tkinter import *
 import tkinter.ttk as ttk
 import analize as an
 
-class App:
+class App(Tk):
 	def __init__(self):
 		self.screen = Tk()
 
-		lableStyle = ttk.Style()
-		lableStyle.configure("Standard.Label", background="#445663", font=("courier", 15, "bold"), foreground ="red")
+		# ------------------- Style --------------------------
+		style = ttk.Style()
+		style.theme_use("alt")
+		style.configure(".", foreground = "white", background = "#445663", borderwidth = 1)
+		style.configure("TButton", background = "#445663")
+		style.map("TButton", foreground = [("pressed", "black"), ("active", "black")])
+		style.configure("TLabelframe", bordercolor = "blue", borderwidth = 1)
+		style.configure("TEntry", foreground = "black")
+		style.configure("TCombobox", foreground = "black")
+		style.configure("Treeview", foreground = "black")
 
 
 		# ---------------- Basic config ----------------------
@@ -15,43 +23,56 @@ class App:
 		self.screen.config(bg="black")
 
 		# Menu for More 
-		self.main_frame = Frame(self.screen,)
+		self.main_frame = Frame(self.screen, bg = "#445663")
 		self.main_frame.pack()
 
 		# Menu for excel configue nad chart
-		self.left_frame = Frame(self.main_frame)
+		self.left_frame = Frame(self.main_frame, bg = "#445663")
 		self.left_frame.grid(row = 0, column = 0)
 
-		self.widget_frame = ttk.LabelFrame(self.left_frame, text = "TEST TEXT", style = "Standard.Label")
+		# ---------- Excel Options ----------
+		self.widget_frame = ttk.Labelframe(self.left_frame, text = "Excel Options")
 		self.widget_frame.grid(row = 0, column = 0, padx = 10, pady = (0, 10))
 
 		# Entry fields
-		self.file_name = self.createEntry(self.widget_frame, "Excel Name", 0, 0, 10, (0, 10))
-		self.cells = self.createEntry(self.widget_frame, "Cells range", 0, 1, 10, (0, 10))
+		self.file_name = self.createEntry(self.widget_frame, "Excel Name", 0, 0, 10, (5, 10))
+		self.cells = self.createEntry(self.widget_frame, "Cells range", 0, 1, 10, (5, 10))
 		self.min = self.createEntry(self.widget_frame, "Min points", 1, 0, 10, (0, 10))
 		self.max = self.createEntry(self.widget_frame, "Max points", 1, 1, 10, (0, 10))
-	
-		self.widget_frame_graph = ttk.LabelFrame(self.left_frame, text = "Chart Options", style = "Standard.Label")
-		self.widget_frame_graph.grid(row = 1, column = 0, padx = 10, pady = (0, 10))
 
-		# Entry fields
-		self.chart_type = self.createEntry(self.widget_frame_graph, "Chart Type", 0, 0, 10, (0, 10))
-		self.marker = self.createEntry(self.widget_frame_graph, "Point marker", 0, 1, 10, (0, 10))
-		self.column_names = self.createEntry(self.widget_frame_graph, "Columns names", 1, 0, 10, (0, 10), 2, sticky = "nsew")
+		self.column_names = self.createEntry(self.widget_frame, "Columns names", 2, 0, 10, (0, 10), 2, sticky = "nsew")
 
+		self.button = ttk.Button(self.widget_frame, text = "Accept", command = self.accepted_excel)
+		self.button.grid(row = 3, column = 0, sticky = "nsew", padx = 10, pady = (0, 10), columnspan = 2)
 		
+		# --------- Chart Options ------------
+		self.widget_frame_graph = ttk.Labelframe(self.left_frame, text = "Chart Options")
+		self.widget_frame_graph.grid(row = 1, column = 0, padx = 10, pady = (5, 10))
 
-		self.widget_frame_settings = ttk.LabelFrame(self.left_frame, text = "Chart Options", style = "Standard.Label")
+		chart_type_text = ttk.Label(self.widget_frame_graph, text = "Chart Type: ", background = "#445663")
+		chart_type_text.grid(row = 0, column = 0, padx = 10, pady = (5, 10))
+		self.chart_type = self.createCombolist(self.widget_frame_graph, ["Plot", "Scatter"], 0, 1, 10, (5, 10))
+
+		chart_marker_text = ttk.Label(self.widget_frame_graph, text = "Marker: ", background = "#445663")
+		chart_marker_text.grid(row = 1, column = 0, padx = 10, pady = (0, 10))
+		self.marker = self.createCombolist(self.widget_frame_graph, ["*", "o"], 1, 1, 10, (0, 10))
+
+	
+		self.xValues_entry = self.createCombolist(self.widget_frame_graph, ["x values"], 2, 0, 10, (0, 10), columnspan = 2, sticky = "nsew")
+		self.yValues_entry = self.createCombolist(self.widget_frame_graph, ["y values"],3, 0, 10, (0, 10), columnspan = 2, sticky = "nsew")
+
+		self.button_chr_option = ttk.Button(self.widget_frame_graph, text = "Accept", state = DISABLED, command = self.accepted_chart)
+		self.button_chr_option.grid(row = 4, column = 0, sticky = "nsew", padx = 10, pady = (0, 5), columnspan = 2)
+
+		# Buttons
+		self.widget_frame_settings = ttk.Labelframe(self.left_frame, borderwidth = 0)
 		self.widget_frame_settings.grid(row = 2, column = 0, padx = 10, pady = (0, 10))
 
-		self.button = ttk.Button(self.widget_frame_settings, text = "Accept", command = self.accepted)
-		self.button.grid(row = 0, column = 0, sticky = "nsew", padx = 10)
-
 		self.button_chart = ttk.Button(self.widget_frame_settings, text = "Create Chart", command = self.createChart)
-		self.button_chart.grid(row = 0, column = 1, sticky = "nsew")
+		self.button_chart.grid(row = 0, column = 0, sticky = "nsew")
 
 		# ------------ Data window --------------
-		self.right_frame = Frame(self.main_frame)
+		self.right_frame = Frame(self.main_frame, bg = "#445663")
 		self.right_frame.grid(row = 0, column = 1)
 
 		
@@ -59,35 +80,51 @@ class App:
 		self.dataScroll.pack(side = "right", fill = "y")
 
 		self.cols = ["Columns names"]
-		self.dataWindow = ttk.Treeview(self.right_frame, show = "headings", yscrollcommand = self.dataScroll.set, columns = self.cols, height = 13)
+		self.dataWindow = ttk.Treeview(self.right_frame, show = "headings", yscrollcommand = self.dataScroll.set, columns = self.cols, height = 20)
 		self.dataWindow.pack()
 		self.dataScroll.config(command=self.dataWindow.yview)
-
-
-
 
 		self.screen.mainloop()
 
 	def createEntry(self, root, defaultInsert, row, column, padx, pady, columnspan = 1, rowspan = 1,sticky = ""):
-		new_entry = ttk.Entry(root)
+		new_entry = ttk.Entry(root, background = "#445663")
 		new_entry.insert(0, defaultInsert)
 		new_entry.bind("<FocusIn>", lambda e: new_entry.delete("0", "end"))
 		new_entry.grid(row = row, column = column, padx = padx, pady = pady, columnspan = columnspan, rowspan = rowspan, sticky = sticky)
 		return new_entry
 
-	def accepted(self):
+	def createCombolist(self, root, combo_list, row, column, padx, pady, columnspan = 1, rowspan = 1,sticky = ""):
+		new_combo = ttk.Combobox(root, values = combo_list)
+		new_combo.current(0)
+		new_combo.grid(row = row, column = column, padx = padx, pady = pady, columnspan = columnspan, rowspan = rowspan, sticky = sticky)
+		return new_combo
+
+	def accepted_excel(self):
 		# Basic 
 		self.file = self.file_name.get() # Getting a file name
 		self.cells_range = self.cells.get()
 
-		# Chart
-		self.type = self.chart_type.get()
-		self.point_marker = self.marker.get()
+		self.min_val = self.min.get() 
+		self.max_val = self.max.get()
 
 		self.cols = self.column_names.get().split(", ")
 		self.dataWindow.config(columns = self.cols)
 
+		self.xValues_entry.config(values = self.cols)
+		self.xValues_entry.current(0)
+		self.yValues_entry.config(values = self.cols)
+		self.yValues_entry.current(0)
+
+		self.button_chr_option.config(state = NORMAL)
+
 		self.load_data()
+
+	def accepted_chart(self):
+		self.type = self.chart_type.get()
+		self.point_marker = self.marker.get()
+
+		self.xValues = self.xValues_entry.get().split(", ")
+		self.yValues = self.yValues_entry.get().split(", ")
 
 	def load_data(self):
 		excel = an.openExcel(self.file)
@@ -95,7 +132,6 @@ class App:
 		#print(self.data)
 
 		#Lodaing data to data window
-
 		for col in self.cols:
 			self.dataWindow.heading(col, text = col)
 
@@ -104,5 +140,6 @@ class App:
 			self.dataWindow.insert("", END , values = new_list)
 
 	def createChart(self):
-		an.scatterData(self.data["Serial Nr."], self.data["Value"], "Data Graph", "o")
-		an.showPlot()
+		if self.type == "Scatter":
+			an.scatterData(self.data["Serial Nr."], self.data["Value"], "Data Graph", "o")
+			an.showPlot()
--- scrips/analize.py
+++ scrips/analize.py
@@ -8,8 +8,20 @@ def openExcel(fileName):
 def lookForColms(data, columsName):
 	return pd.DataFrame(data, columns = columsName)
 
-def scatterData(xValues, yValues, labelName = "Yes", marker = "*"):
+def scatterData(xValues, yValues, labelName = "", marker = "*"):
 	plt.scatter(xValues, yValues, label = labelName, marker = marker)
+
+def plotData(xValues, yValues, labelName = "", marker = "*"):
+	pass
 
 def showPlot():
 	plt.show()
+
+
+if __name__ == "__main__":
+	data = openExcel("C:\\Users\\Mateusz\\Desktop\\simpleData\\data1.xlsx")
+	dataFrame = lookForColms(data, ["Nr", "Value", "Serial Nr."])
+
+	scatterData(dataFrame["Value"], dataFrame["Value"])
+
+	showPlot()
